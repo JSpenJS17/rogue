3d12d5a91c5eb30e2395b5ee2b0a4bab454aa68e
jspenjs17 <jspencer.js17@gmail.com>
2025-04-03 04:14:22 -0500

    Add 1 to MAXOBJ to account for no diagonal movement

--- rogue.h
+++ rogue.h
@@ -21,7 +21,7 @@
  */
 #define MAXROOMS    9
 #define MAXTHINGS   9
-#define MAXOBJ      9  /* number of object spawn attempts per level */
+#define MAXOBJ      10  /* number of object spawn attempts per level */
 #define MAXPACK     23
 #define MAXTRAPS    10
 #define AMULETLEVEL 26
