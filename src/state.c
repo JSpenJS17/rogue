e5d004654e1aad2bd45583bbca2c67675e4ff5af
jspenjs17 <jspencer.js17@gmail.com>
2025-04-14 21:31:08 -0500

    Fix restoring saves

--- src/state.c
+++ src/state.c
@@ -2342,7 +2342,7 @@ int rs_restore_file (FILE *inf)
     rs_read_int (inf, &vf_hit);
     rs_read_int (inf, &dnum);
     rs_read_int (inf, &seed);
-    rs_read_ints (inf, e_levels, 21);
+    rs_read_ints (inf, e_levels, 20);
     rs_read_coord (inf, &delta);
     rs_read_coord (inf, &oldpos);
     rs_read_coord (inf, &stairs);
